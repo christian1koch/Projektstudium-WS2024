a94d7709ab1f3791133fd25fb91d7e1c330071d0
Christian Koch Echeverria <ko.che@web.de>
2024-11-04 22:32:10 +0100

    initial project setup

---
+++ Musik-App/Others/Musik_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Musik_AppApp.swift
+//  Musik-App
+//
+//  Created by Christian Koch Echeverria on 04.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Musik_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
