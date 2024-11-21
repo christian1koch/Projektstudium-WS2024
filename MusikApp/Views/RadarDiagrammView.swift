cb1d391d51d99bc533f733c19e9c04ff23ba9094
Christian Koch Echeverria <ko.che@web.de>
2024-11-21 20:46:43 +0100

    6: Implemented Radar Diagramm

--- MusikApp/Views/RadarDiagrammView.swift
+++ MusikApp/Views/RadarDiagrammView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import DDSpiderChart
 
 struct NamedValue: Hashable {
     let id: Int;
@@ -14,28 +15,49 @@ struct NamedValue: Hashable {
     let value: Int
 }
 
-let MOCK_VALUES = [
-    NamedValue(id: 1, label: "Speed", value: 80),
-    NamedValue(id: 2, label: "Accuracy", value: 90),
-    NamedValue(id: 3, label: "Agility", value: 70)
-]
-
 struct RadarDiagrammView: View {
-    let namedValuesArray: [NamedValue]
+    var namedValuesArray: [NamedValue]
+    
+    var axesLabels: [String] {
+        return namedValuesArray.map({$0.label})
+    }
+    var diagramValues: [Float] {
+        return namedValuesArray.map({Float($0.value) / 100})
+    }
     var body: some View {
-        
-        VStack {
 
+        VStack {
             List(namedValuesArray, id: \.id) { namedValue in
                     Text("\(namedValue.label), \(namedValue.value)")
-
             }
+            DDSpiderChart(
+                axes: axesLabels,
+                values: [
+                    DDSpiderChartEntries(values: diagramValues, color: .green),
+                ],
+                fontTitle: .boldSystemFont(ofSize: 16),
+                textColor: .black
+            ).frame(width: 300, height: 300)
         }
         .padding()
     }
     
 }
 
+// Mock Values for Preview
+private let MOCK_VALUES = [
+    NamedValue(id: 1, label: "Danceability", value: 80),
+    NamedValue(id: 2, label: "Energy", value: 90),
+    NamedValue(id: 3, label: "Speechiness", value: 30),
+    NamedValue(id: 4, label: "Acousticness", value: 40),
+    NamedValue(id: 5, label: "Instrumentalness", value: 20),
+    NamedValue(id: 6, label: "Liveness", value: 70),
+    NamedValue(id: 7, label: "Valence", value: 60)
+]
+
 #Preview {
     RadarDiagrammView(namedValuesArray: MOCK_VALUES)
 }
+
+
+
