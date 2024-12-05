0e38ba0cbcc8d339423ad78c12cc73e8f476a30d
Dennis Forster <66913269+Semanticraft@users.noreply.github.com>
2024-12-05 13:47:19 +0100

    added function to handle microphone permissions
    might need an entry in Info.plist

--- MusikApp/ViewModels/ShazamViewModel.swift
+++ MusikApp/ViewModels/ShazamViewModel.swift
@@ -17,4 +17,31 @@ struct ShazamViewModel: ObservableObject {
         }
         listeningButtonTitle = isListening ? "Stop Listening" : "Start Listening"
     }
+
+    func requestMicrophonePermission(completion: @escaping (Bool) -> Void) {
+    // Check if the microphone permission is already granted
+    switch AVCaptureDevice.authorizationStatus(for: .audio) {
+    case .authorized:
+        // Microphone access is already granted
+        completion(true)
+    case .notDetermined:
+        // Microphone access has not been determined yet, request permission
+        AVCaptureDevice.requestAccess(for: .audio) { response in
+            DispatchQueue.main.async {
+                completion(response)
+            }
+        }
+    case .denied, .restricted:
+        // Microphone access has been denied or is restricted
+        DispatchQueue.main.async {
+            completion(false)
+        }
+    @unknown default:
+        // Handle any other cases (future iOS versions)
+        DispatchQueue.main.async {
+            completion(false)
+        }
+    }
+}
+
 }
