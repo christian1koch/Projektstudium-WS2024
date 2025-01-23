8b895e8fb01ff3b3b0ae851a99b0d52d0bfc124a
idlehand <trefzer.christian@gmail.com>
2025-01-23 16:12:35 +0100

    added LineUpView

---
+++ MusikApp/Views/LineUpView.swift
@@ -0,0 +1,39 @@
+//
+//  LineUpView.swift
+//  MusikApp
+//
+//  Created by Christian Trefzer on 23.01.25.
+//
+
+import SwiftUI
+
+struct LineUpView: View {
+    var room: Room
+
+    var body: some View {
+        VStack(spacing: 16) {
+            Text(room.id ?? "Unknown Room ID")
+                .htwContainerStyle()
+                .font(.headline)
+
+            Text("Spieler")
+                .htwTitleStyle()
+
+            ForEach(Array(room.players ?? []), id: \.self) { player in
+                Text(player.name ?? "Unknown")
+                    .padding()
+                    .htwContainerStyle()
+            }
+
+            Button("showtime") {
+                // TODO: Add action
+            }
+            .buttonStyle(.htwPrimary)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LineUpView(room: Room(id: "1234", host: Player(name: "Host"), players: Set([Player(name: "Klaus"), Player(name: "Gabi")]), rounds: nil, settings: Settings(mode: .FASTEST_STOPS, maxPlayers: 2, isPublic: false), status: nil, activeRound: nil))
+}
