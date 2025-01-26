be053473275177ae3b29d7cd4f3d4f30d52016a9
idlehand <trefzer.christian@gmail.com>
2025-01-27 00:24:56 +0100

    added navigation in BackstageView

--- MusikApp/Views/BackstageView.swift
+++ MusikApp/Views/BackstageView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct BackstageView: View {
-    @State private var playerName: String = ""
+    @State var playerName: String = ""
+    @State var navigateToStageSetup = false
+    @State var navigateToPrivateStage = false
+    @State var navigateToPublicStage = false
+    
+    private var gameController: GameController = GameController.shared
+
     
     var body: some View {
         NavigationStack {
@@ -25,23 +31,72 @@ struct BackstageView: View {
                 }
                 .htwContainerStyle()
                 
-                // Add player button
-                Button("host a stage") {
-                    // Add player action
+                // Host game button
+                Button("host stage") {
+                    if isUserNameValid() {
+                        let player = Player(name: playerName, points: 0, ready: false)
+                        gameController.player = player
+                        
+                        // navigate to stage setup
+                        navigateToStageSetup = true
+                        NavigationLink(destination: StageSetupView()) {     //FIX This
+                        }
+                    }
                 }
                 .buttonStyle(.htwPrimary)
+                .disabled(!isUserNameValid())
                 
-                // Join a game
-                Button("join a stage") {
-                    // Join game action
+                // Join private game button
+                Button("join pivate stage") {
+                    if isUserNameValid() {
+                        let player = Player(name: playerName, points: 0, ready: false)
+                        gameController.player = player
+                        // navigate to private stage
+                        navigateToPrivateStage = true
+                        NavigationLink(destination: PrivateJoinView()) {    //FIX This
+                        }
+                    }
                 }
                 .buttonStyle(.htwPrimary)
+                .disabled(!isUserNameValid())
+                
+                // Join public game button
+                Button("join public stage") {
+                    if isUserNameValid() {
+                        let player = Player(name: playerName, points: 0, ready: false)
+                        gameController.player = player
+                        
+                        // navigate to private stage
+                        navigateToPublicStage = true
+                        NavigationLink(destination: PublicJoinView()) {     //FIX This
+                        }
+                    }
+                }
+                .buttonStyle(.htwPrimary)
+                .disabled(!isUserNameValid())
+                
+                // Navigation Links (triggered by state changes)
+                NavigationLink(destination: StageSetupView(), isActive: $navigateToStageSetup) {
+                    EmptyView()
+                }
+                NavigationLink(destination: PrivateJoinView(), isActive: $navigateToPrivateStage) {
+                    EmptyView()
+                }
+                NavigationLink(destination: PublicJoinView(), isActive: $navigateToPublicStage) {
+                    EmptyView()
+                }
             }
-            
+            .padding()
         }
         
     }
+    
+    // checks if user name is valide
+    private func isUserNameValid() -> Bool {
+        return !playerName.isEmpty
+    }
 }
+
 
 #Preview {
     BackstageView()
